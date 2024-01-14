0091856942a804820403066bb5512a72289d5c8a
Dan Rosu <cristian.rosu@student.upt.ro>
2024-01-14 14:18:14 +0200

    add product-category-menu component and product-category class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,29 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule} from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+
+
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: 'category/:id', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'} //this wildcard will match on anything that didn't match above routes
+
+  //order of the routes is important
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,25 +3,44 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products?size=100';
+  private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
-    getProductList(): Observable<Product[]> {
-      return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    getProductList(theCategoryId: number): Observable<Product[]> {
+
+      const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+      return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
         map(response => response._embedded.products)
+      );
+    }
+
+    //calling REST API
+    //Maps the JSON data from Spring Data REST to productCategory array
+    getProductCategories(): Observable<ProductCategory[]> {
+      return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+        map(response => response._embedded.productCategory)
       );
     }
   }
 
-interface GetResponse{
+interface GetResponseProducts{
   _embedded: {
     products: Product[];
+  }
+}
+
+//unwraps the JSON from Spring Data REST _embedded entry
+interface GetResponseProductCategory{
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
