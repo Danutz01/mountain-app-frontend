0db5ae0e3654d3b278545906ba55989d2bbf0556
Dan Rosu <cristian.rosu@student.upt.ro>
2024-01-29 00:32:45 +0200

    Add to cart from detail product view

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -20,14 +20,8 @@ export class CartService {
     let existingCartItem: CartItem |undefined = undefined;
 
     if(this.cartItems.length > 0){
-    //find the item in the cart based on item id
-      
-      for(let tempCartItem of this.cartItems){
-        if(tempCartItem.id === theCartItem.id){
-          existingCartItem = tempCartItem;
-          break;
-        }
-      }
+      //find the item in the cart based on item id
+      existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id === theCartItem.id);
 
       //check if we found it
       alreadyExistsInCart = (existingCartItem != undefined);
