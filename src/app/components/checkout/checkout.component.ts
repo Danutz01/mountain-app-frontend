7669680df186f828e2b7ddc7b74f163aef45a80c
Dan Rosu <cristian.rosu@student.upt.ro>
2024-03-08 17:45:39 +0200

    create new classes for Countries and States

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { toJSDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-calendar';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
+import { MeetmountainFormService } from 'src/app/services/meetmountain-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -15,15 +18,25 @@ export class CheckoutComponent implements OnInit {
 
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) { }
+  creditCardYears: number[] = [];
+  creditCardMoths: number[] = [];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
+  countries: Country[] = [];
+  
+  constructor(private formBuilder: FormBuilder,
+              private meetmountainFormService: MeetmountainFormService) { }
 
   ngOnInit(): void {
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
+        //validate email, match any comination
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -48,14 +61,43 @@ export class CheckoutComponent implements OnInit {
         expirationYear: ['']
       })
     });
+
+    //populam cardul cu luni
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("startMonth: " + startMonth);
+
+    this.meetmountainFormService.getCreditCardMoths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card moths: " + JSON.stringify(data));
+        this.creditCardMoths = data;
+      }
+    )
+
+    //populam cardul cu ani
+    this.meetmountainFormService.getCreditCardYears().subscribe(
+      data => {
+        console.log("Retrieved cred card years: " + JSON.stringify(data));
+        this.creditCardYears = data;
+      }
+    )
+
+    //populam tarile
+    this.meetmountainFormService.getCountries().subscribe(
+      data => {
+        console.log("Retrieved countries: " + JSON.stringify(data));
+        this.countries = data;
+      }
+    )
   }
 
   copyShippingAddressToBillingAddress(event: Event) {
     // Use a type guard to narrow down the type of the event target
     if (event.target instanceof HTMLInputElement && event.target.checked) {
       this.checkoutFormGroup.controls['billingAddress'].setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+      this.billingAddressStates = this.shippingAddressStates;
     } else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
+      this.billingAddressStates = [];
     }
   }
   
@@ -64,6 +106,52 @@ export class CheckoutComponent implements OnInit {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log("The email address is " + this.checkoutFormGroup.get('customer')?.value.email);
+    console.log("The shipping address country is: " + this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
+    console.log("The shipping address state is: " + this.checkoutFormGroup.get('shippingAddress')?.value.state.name);
+  }
+
+  handleMonthsAndYears(){
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+
+    let startMonth: number;
+
+    if(currentYear === selectedYear){
+      startMonth = new Date().getMonth()+1;
+    }else{
+      startMonth = 1;
+    }
+
+    this.meetmountainFormService.getCreditCardMoths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMoths = data;
+      }
+    )
+  }
+
+  getStates(formGroupName: string){
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.meetmountainFormService.getStates(countryCode).subscribe(
+      data => {
+        if(formGroupName === 'shippingAddress'){
+          this.shippingAddressStates = data;
+        }else{
+          this.billingAddressStates = data;
+        }
+
+        formGroup?.get('state')?.setValue(data[0]);
+      }
+    )
   }
 
 }
--- src/app/services/meetmountain-form.service.ts
+++ src/app/services/meetmountain-form.service.ts
@@ -1,12 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
+import { Country } from '../common/country';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeetmountainFormService {
 
-  constructor() { }
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statesUrl = 'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) { }
 
   getCreditCardMoths(startMonth: number): Observable<number[]>{
     let data: number[] = [];
@@ -31,5 +37,32 @@ export class MeetmountainFormService {
     }
 
     return of(data);
+  }
+
+  getCountries(): Observable<Country[]>{
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]>{
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    )
+  }
+}
+
+//unwraps the JSON from Spring Data REST _embedded entry
+interface GetResponseCountries {
+  _embedded:{
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates {
+  _embedded:{
+    states: State[];
   }
 }
